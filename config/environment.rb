4bd2cd7e6c743046d2f63d098b929dc66a5bd107
Joshua <mwalejoshua89@gmail.com>
2022-08-24 13:26:25 +0300

    REDO ORMS weekend

--- config/environment.rb
+++ config/environment.rb
@@ -6,3 +6,22 @@ require_relative '../lib/student'
 DB = { conn: SQLite3::Database.new("db/school.db") }
 
 # RUN CODE FROM HERE
+
+#CREATE TABLE
+Student.create_table
+
+#Create a new student
+joshua = Student.new(name: "Joshua", age: 18)
+#add  student to database
+joshua.add_to_db
+pp Student.show_all
+
+joshua.name = "Water Bottle"
+joshua.age = 2
+joshua.update_student
+
+pp Student.show_all
+
+joshua.delete_student
+pp Student.show_all
+
